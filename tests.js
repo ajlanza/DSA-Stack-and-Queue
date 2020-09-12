f2e87bea6444fcd3d8cf8567637dc6274275681e
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-09-12 14:16:26 -0400

    added starTrek function to tests

---
+++ tests.js
@@ -0,0 +1,13 @@
+const Stack = require('./stack');
+
+function starTrek(){
+  const starTrek = new Stack();
+  starTrek.push('Kirk');
+  starTrek.push('Spock');
+  starTrek.push('McCoy');
+  starTrek.push('Scotty');
+
+  return starTrek;
+}
+
+console.log(starTrek());
